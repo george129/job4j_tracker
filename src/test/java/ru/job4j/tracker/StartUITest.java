af70edf25801b98001611f2175e37e52c148af74
user <none>
2020-06-27 01:32:27 +0500

    9. Написать тесты на StartUI. [#282470]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,18 +7,19 @@ public class StartUITest {
 
     @Test
     public void createItem() {
-        String[] answer = {"Just for test", "List", "All", "Items"};
+        String[] answer = {"0", "Just for test", "0", "List", "0", "All", "0", "Items", "1"};
         Input in = new StubInput(answer);
         Tracker tr = new Tracker();
-        StartUI.createItem(in, tr);
-        StartUI.createItem(in, tr);
-        StartUI.createItem(in, tr);
-        StartUI.createItem(in, tr);
+        UserAction[] acts = {
+                new CreateAction(),
+                new ExitAction()
+        } ;
+        new StartUI().init(in, tr, acts);
         Item[] created = tr.findAll();
-        Assert.assertEquals(created[0].getName(), answer[0]);
-        Assert.assertEquals(created[1].getName(), answer[1]);
-        Assert.assertEquals(created[2].getName(), answer[2]);
-        Assert.assertEquals(created[3].getName(), answer[3]);
+        Assert.assertEquals(created[0].getName(), answer[1]);
+        Assert.assertEquals(created[1].getName(), answer[3]);
+        Assert.assertEquals(created[2].getName(), answer[5]);
+        Assert.assertEquals(created[3].getName(), answer[7]);
     }
 
     @Test
@@ -26,28 +27,37 @@ public class StartUITest {
         Tracker tr = new Tracker();
         Item it = new Item("test");
         tr.add(it);
-        String[] answers = {it.getId(), "edited test"};
-        StartUI.editItem(new StubInput(answers), tr);
-        Assert.assertEquals(answers[1], tr.findById(answers[0]).getName());
+        String[] answers = {"0", it.getId(), "edited test", "1"};
+        Input in = new StubInput(answers);
+        UserAction[] acts = {
+                new EditAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tr, acts);
+        Assert.assertEquals(answers[2], tr.findById(answers[1]).getName());
     }
 
     @Test
     public void deleteItem() {
         Tracker tr = new Tracker();
         Item[] items = new Item[3];
-        items[0] = new Item("test1");
-        items[1] = new Item("test2");
-        items[2] = new Item("test3");
-        tr.add(items[0]);
-        tr.add(items[1]);
-        tr.add(items[2]);
+        tr.add(new Item("test1"));
+        String deletedId = tr.add(new Item("test2")).getId();
+        tr.add(new Item("test3"));
+        String[] answers = {"0", deletedId, "1"};
+        Input in = new StubInput(answers);
+        UserAction[] acts = {
+                new DeleteAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tr, acts);
         Item[] itemsAfterEdit;
-        tr.delete(items[1].getId());
+        tr.delete(deletedId);
         itemsAfterEdit = tr.findAll();
         Assert.assertEquals(2, itemsAfterEdit.length);
-        Assert.assertEquals(itemsAfterEdit[0].getName(), items[0].getName());
-        Assert.assertEquals(itemsAfterEdit[1].getName(), items[2].getName());
-        Assert.assertEquals(null, tr.findById(items[1].getId()));
+        Assert.assertEquals("test1", itemsAfterEdit[0].getName());
+        Assert.assertEquals("test3", itemsAfterEdit[1].getName());
+        Assert.assertEquals(null, tr.findById(deletedId));
     }
 
     @Test
