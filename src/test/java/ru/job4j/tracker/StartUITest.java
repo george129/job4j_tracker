d4af1eb364df26b97f96aaa89453b6c137728e2c
user <none>
2020-07-03 18:27:17 +0500

    9.2. Тесты вывода на консоль в StartUI [#282457]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,14 +7,15 @@ public class StartUITest {
 
     @Test
     public void createItem() {
+        Output out = new StubOutput();
         String[] answer = {"0", "Just for test", "0", "List", "0", "All", "0", "Items", "1"};
         Input in = new StubInput(answer);
         Tracker tr = new Tracker();
         UserAction[] acts = {
-                new CreateAction(),
-                new ExitAction()
+                new CreateAction(out),
+                new ExitAction(out)
         } ;
-        new StartUI().init(in, tr, acts);
+        new StartUI(out).init(in, tr, acts);
         Item[] created = tr.findAll();
         Assert.assertEquals(created[0].getName(), answer[1]);
         Assert.assertEquals(created[1].getName(), answer[3]);
@@ -24,21 +25,23 @@ public class StartUITest {
 
     @Test
     public void editItem() {
+        Output out = new StubOutput();
         Tracker tr = new Tracker();
         Item it = new Item("test");
         tr.add(it);
         String[] answers = {"0", it.getId(), "edited test", "1"};
         Input in = new StubInput(answers);
         UserAction[] acts = {
-                new EditAction(),
-                new ExitAction()
+                new EditAction(out),
+                new ExitAction(out)
         };
-        new StartUI().init(in, tr, acts);
+        new StartUI(out).init(in, tr, acts);
         Assert.assertEquals(answers[2], tr.findById(answers[1]).getName());
     }
 
     @Test
     public void deleteItem() {
+        Output out = new StubOutput();
         Tracker tr = new Tracker();
         Item[] items = new Item[3];
         tr.add(new Item("test1"));
@@ -47,10 +50,10 @@ public class StartUITest {
         String[] answers = {"0", deletedId, "1"};
         Input in = new StubInput(answers);
         UserAction[] acts = {
-                new DeleteAction(),
-                new ExitAction()
+                new DeleteAction(out),
+                new ExitAction(out)
         };
-        new StartUI().init(in, tr, acts);
+        new StartUI(out).init(in, tr, acts);
         Item[] itemsAfterEdit;
         tr.delete(deletedId);
         itemsAfterEdit = tr.findAll();
@@ -61,10 +64,59 @@ public class StartUITest {
     }
 
     @Test
+    public void findAll() {
+        Output out = new StubOutput();
+        Tracker tr = new Tracker();
+        tr.add(new Item("test1"));
+        tr.add(new Item("test2"));
+        tr.add(new Item("test3"));
+        Input in = new StubInput(new String[]{"0","1"});
+        new StartUI(out).init(in, tr, new UserAction[]{new ListAction(out), new ExitAction(out)});
+        System.out.println(out.toString());
+    }
+
+    @Test
     public void findItemById() {
+        Output out = new StubOutput();
+        Tracker tr = new Tracker();
+        Item t1 = new Item("first test");
+        Item t2 = new Item("second test");
+        Item t3 = new Item("third test");
+        tr.add(t1);
+        tr.add(t2);
+        tr.add(t3);
+        Input in = new StubInput(new String[]{
+                "0",
+                t2.getId(),
+                "1"
+        });
+        new StartUI(out).init(in, tr, new UserAction[] {
+                new FindByIdAction(out), new ExitAction(out)
+        });
+//????
+        Assert.assertEquals(t2, tr.findById(t2.getId()));
     }
 
     @Test
     public void findItemByName() {
+        Tracker tr = new Tracker();
+        Item t1 = new Item("first test");
+        Item t2 = new Item("second test");
+        Item t3 = new Item("third test");
+        tr.add(t1);
+        tr.add(t2);
+        tr.add(t3);
+        Assert.assertEquals(t2, tr.findByName(t2.getName())[0]);
+    }
+
+    @Test
+    public void exit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"0"});
+        Tracker tr = new Tracker();
+        UserAction[] acts = {new ExitAction(out)};
+        new StartUI(out).init(in, tr, acts);
+        String ls = System.lineSeparator();
+        Assert.assertEquals("Menu"+ls+"0.Exit"+ls+"Enter choice number: "+"Exiting..."+ls, out.toString());
     }
 }
