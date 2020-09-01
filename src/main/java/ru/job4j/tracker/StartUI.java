99236b4cd3597acbf4ffd55f232c66953bfca80e
user <none>
2020-09-01 22:17:12 +0500

    6. Изменить программу Tracker из 2-го модуля [#282442]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StartUI {
     private final Output out;
 
@@ -7,24 +10,24 @@ public class StartUI {
         out = o;
     }
 
-    public void init(Input in, Tracker tr, UserAction[] action) {
+    public void init(Input in, Tracker tr, List<UserAction> action) {
         boolean run = true;
         while (run) {
             this.showMenu(action);
             int select = in.askInt("");
-            if (select < 0 || select > action.length - 1) {
-                out.println("Wrong menu number. From 0 to " + (action.length - 1) + " allowed");
+            if (select < 0 || select > action.size() - 1) {
+                out.println("Wrong menu number. From 0 to " + (action.size() - 1) + " allowed");
                 continue;
             }
-            run = action[select].execute(in, tr);
+            run = action.get(select).execute(in, tr);
         }
     }
 
 
-    private void showMenu(UserAction[] actions) {
+    private void showMenu(List<UserAction> actions) {
         out.println("Menu");
-        for (int i = 0; i < actions.length; i++) {
-            out.println(i + "." + actions[i].name());
+        for (int i = 0; i < actions.size(); i++) {
+            out.println(i + "." + actions.get(i).name());
         }
         out.print("Enter choice number: ");
     }
@@ -33,15 +36,14 @@ public class StartUI {
         Output output = new ConsoleOutput();
         Input input = new ValidateInput(output, new ConsoleInput());
         Tracker tr = new Tracker();
-        UserAction[] actions = {
-                new CreateAction(output),
-                new ListAction(output),
-                new EditAction(output),
-                new DeleteAction(output),
-                new FindByIdAction(output),
-                new FindByNameAction(output),
-                new ExitAction(output)
-        };
+        List<UserAction> actions = new ArrayList<UserAction>();
+        actions.add(new CreateAction(output));
+        actions.add(new ListAction(output));
+        actions.add(new EditAction(output));
+        actions.add(new DeleteAction(output));
+        actions.add(new FindByIdAction(output));
+        actions.add(new FindByNameAction(output));
+        actions.add(new ExitAction(output));
         new StartUI(output).init(input, tr, actions);
     }
 }
