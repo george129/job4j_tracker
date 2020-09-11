e6e6cf343d358781a9911a4261ac966f2f6738ca
user <none>
2020-09-11 14:37:29 +0500

    0. Сортировка [#282435]

--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,15 +20,17 @@ public class ItemTest {
         itemList.add(new Item("Three"));
         itemList.add(new Item("Four"));
         Collections.sort(itemList, new ItemComparatorAsc());
-        assertEquals("Four", itemList.get(0).getName());
-        assertEquals("One", itemList.get(1).getName());
-        assertEquals("Three", itemList.get(2).getName());
-        assertEquals("Two", itemList.get(3).getName());
+        assertEquals(Arrays.asList(new Item("Four"), new Item("One"), new Item("Three"), new Item("Two")), itemList);
+//        assertEquals("Four", itemList.get(0).getName());
+//        assertEquals("One", itemList.get(1).getName());
+//        assertEquals("Three", itemList.get(2).getName());
+//        assertEquals("Two", itemList.get(3).getName());
         Collections.sort(itemList, new ItemComparatorDesc());
-        assertEquals("Two", itemList.get(0).getName());
-        assertEquals("Three", itemList.get(1).getName());
-        assertEquals("One", itemList.get(2).getName());
-        assertEquals("Four", itemList.get(3).getName());
+        assertEquals(Arrays.asList(new Item("Two"), new Item("Three"), new Item("One"), new Item("Four")), itemList);
+//        assertEquals("Two", itemList.get(0).getName());
+//        assertEquals("Three", itemList.get(1).getName());
+//        assertEquals("One", itemList.get(2).getName());
+//        assertEquals("Four", itemList.get(3).getName());
 
     }
 }
