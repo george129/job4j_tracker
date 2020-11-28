9995ade5f02da4a63701221af87fecd63da6c064
user <none>
2020-11-29 02:43:51 +0500

    0. Stream API [#282300]

---
+++ src/main/java/ru/job4j/streamapi/Filter.java
@@ -0,0 +1,23 @@
+package ru.job4j.streamapi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Filter {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            nums.add((int) (Math.random() * 100 - 50));
+        }
+        for (Integer num : nums) {
+            System.out.print(num + "\t");
+        }
+        System.out.println();
+        List<Integer> positive = nums.stream().filter((ints) -> ints >= 0).
+                collect(Collectors.toList());
+        for (Integer num : positive) {
+            System.out.print(num + "\t");
+        }
+    }
+}
